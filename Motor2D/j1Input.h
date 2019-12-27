09a2d5b36771a79ded8438e3472c78c41bc3d112
Fran Guerrero <franguerreroo98@gmail.com>
2019-12-27 18:39:04 +0100

    Revert "Elementos de UI (No compila)"
    This reverts commit a45c41a0d74ef41a38e428c781e319b72be2e2b4.

--- Motor2D/j1Input.h
+++ Motor2D/j1Input.h
@@ -68,14 +68,6 @@ public:
 	void GetMousePosition(int &x, int &y);
 	void GetMouseMotion(int& x, int& y);
 
-	// Text
-	void EnableTextInput();
-	void DisableTextInput();
-	int GetCursorPosition();
-
-	p2SString GetText();
-	p2SString GetModifiedString();
-
 private:
 	bool		windowEvents[WE_COUNT];
 	j1KeyState*	keyboard;
@@ -84,10 +76,6 @@ private:
 	int			mouse_motion_y;
 	int			mouse_x;
 	int			mouse_y;
-	p2SString	text;
-	bool		text_input = false;
-
-	int			cursor_pos = 0;
 };
 
 #endif // __j1INPUT_H__
